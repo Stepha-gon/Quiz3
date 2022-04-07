6cc36d2445be5655401a07ab2813185918782ced
Stephanie Gonzalez <79724950+Stepha-gon@users.noreply.github.com>
2022-04-06 20:58:25 -0500

    Update Quiz3_GinaGonzalez.sql

--- Quiz3_GinaGonzalez.sql
+++ Quiz3_GinaGonzalez.sql
@@ -1,15 +1,15 @@
 --Presentado por Gina Stephanie Gonzalez Quiz3
-select * from dbo.[Ventas_Videojuegos(1)]
+select * from dbo.[ventas Videojuegos (3)]
 
 
-EXEC sp_rename 'dbo.Ventas_Videojuegos(1).Ventas_NA', 'NorteAmerica', 'COLUMN';
-EXEC sp_rename 'dbo.Ventas_Videojuegos(1).Ventas_EU', 'Europa', 'COLUMN';
-EXEC sp_rename 'dbo.Ventas_Videojuegos(1).Ventas_JP', 'Japon', 'COLUMN';
-EXEC sp_rename 'dbo.Ventas_Videojuegos(1).Ventas_OTROS', 'Otros', 'COLUMN';
+EXEC sp_rename 'dbo.ventas Videojuegos (3).Ventas NA', 'NorteAmerica', 'COLUMN';
+EXEC sp_rename 'dbo.ventas Videojuegos (3).Ventas EU', 'Europa', 'COLUMN';
+EXEC sp_rename 'dbo.ventas Videojuegos (3).Ventas JP', 'Japon', 'COLUMN';
+EXEC sp_rename 'dbo.ventas Videojuegos (3).Ventas OTROS', 'Otros', 'COLUMN';
 
 
 select Nombre,Region, Ventas
-from dbo.[Ventas_Videojuegos(1)]
+from dbo.[ventas Videojuegos (3)]
 unpivot 
 (
     Ventas
